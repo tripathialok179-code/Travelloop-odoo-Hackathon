6f65f414229c2420058deba376643e5ca4e79b0b
Harsh Parmar <hemantdarbar123@gmail.com>
2026-05-10 16:21:35 +0530

    some bug fixes

--- config.py
+++ config.py
@@ -4,15 +4,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-
+    # Use MySQL for industry standard, SQLite for rapid testing
     SQLALCHEMY_DATABASE_URI = os.getenv(
-        'DATABASE_URL',
-        'sqlite:///traveloop.db'
+        'DATABASE_URL', 
+        'mysql+mysqlconnector://root:password@localhost/traveloop'
     )
-
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    SECRET_KEY = os.getenv(
-        'SECRET_KEY',
-        'hackathon-super-secret-key'
-    )
+    SECRET_KEY = os.getenv('SECRET_KEY', 'odoo-hackathon-2026-secret')
